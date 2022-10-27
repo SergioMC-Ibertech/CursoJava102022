79cbb60bb802638c30d55ba09c00346843426607
Sergio Martinez Correa <torettosergy88@gmail.com>
2022-10-27 14:16:02 +0200

    Archivo PruebaGit modificado

--- src/curso/java/PruebaGit.java
+++ src/curso/java/PruebaGit.java
@@ -3,8 +3,7 @@ package curso.java;
 public class PruebaGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hola desde JAVA GitHub");
 	}
 
 }
